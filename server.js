814ffcd73ef3e93f161f684e5f150425c3b44b06
Sravan Karthik <158583651+sravankarthik95@users.noreply.github.com>
2025-11-06 20:02:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use("/images", express.static("public/images"));
+app.use("/images", express.static("images"));
 
 // Route to get mobiles.json
 app.get("/api/mobiles", (req, res) => {
